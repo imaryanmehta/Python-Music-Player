6dc01582aa985f3ddaf6bdc293304ce81d126621
imaryanmehta <imaryanmehta@gmail.com>
2021-06-04 20:39:17 +0530

    Remove while loop to correct else problem.

--- musicplayer.py
+++ musicplayer.py
@@ -4,12 +4,11 @@ _music = input("""Select your music from the given Number of List below:
 Water
 Eye
 Exercise\n""")
-while True:
-    if _music == "water":
-        playsound('C:\\Users\\aarya\\PycharmProjects\\PythonTut\\water.mp3')
-    elif _music == "eye":
-        playsound('C:\\Users\\aarya\\PycharmProjects\\PythonTut\\eye.mp3')
-    elif _music == "exercise":
-        playsound('C:\\Users\\aarya\\PycharmProjects\\PythonTut\\exercise.mp3')
-    else:
-        print("Sorry This Song is not Exist try again!!")
+if _music == "water":
+    playsound('C:\\Users\\aarya\\PycharmProjects\\PythonTut\\water.mp3')
+elif _music == "eye":
+    playsound('C:\\Users\\aarya\\PycharmProjects\\PythonTut\\eye.mp3')
+elif _music == "exercise":
+    playsound('C:\\Users\\aarya\\PycharmProjects\\PythonTut\\exercise.mp3')
+else:
+    print("Sorry This Song is not Exist try again!!")
