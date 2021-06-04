74b1e647bc0c59e0c258d5b9c738124d2cdef7db
imaryanmehta <imaryanmehta@gmail.com>
2021-06-04 18:00:23 +0530

    Python Music Player

---
+++ musicplayer.py
@@ -0,0 +1,15 @@
+from playsound import playsound
+print("\t\t\t\t\tWelcome to Python Music Player")
+_music = input("""Select your music from the given Number of List below:
+Water
+Eye
+Exercise\n""")
+while True:
+    if _music == "water":
+        playsound('C:\\Users\\aarya\\PycharmProjects\\PythonTut\\water.mp3')
+    elif _music == "eye":
+        playsound('C:\\Users\\aarya\\PycharmProjects\\PythonTut\\eye.mp3')
+    elif _music == "exercise":
+        playsound('C:\\Users\\aarya\\PycharmProjects\\PythonTut\\exercise.mp3')
+    else:
+        print("Sorry This Song is not Exist try again!!")
